a95748b8b00eeaad4d159ad585bf1a9dad936434
Brien Pafford <bcpaff@gmail.com>
2015-07-31 10:24:55 -0500

    Added app, server files, and set up web scraping calls

---
+++ app.js
@@ -0,0 +1,6 @@
+var path = require('path');
+
+var server = require(path.join(process.cwd(), '/lib/server'));
+var port = process.env.PORT || 1337;
+server(port);
+console.log('server running on http://localhost:1337');
